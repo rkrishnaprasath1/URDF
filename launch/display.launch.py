fa6538626a279648d051f60a2db64113a2a76484
rkrishnaprasath1 <rkrishnaprasath1@gmail.com>
2025-11-05 12:16:37 +0530

    Update display.launch.py

--- launch/display.launch.py
+++ launch/display.launch.py
@@ -1,17 +1,30 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+import os
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+    package_name = 'urdf_demo'
+    urdf_file_name = 'car_demo.urdf'
+
+    urdf_path = os.path.join(
+        get_package_share_directory(package_name),
+        'urdf',
+        urdf_file_name)
+
+    with open(urdf_path, 'r') as infp:
+        robot_desc = infp.read()
+
     return LaunchDescription([
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
-            arguments=[{'robot_description': open(urdf_path).read()}],
-            output='screen'
+            output='screen',
+            parameters=[{'robot_description': robot_desc}]
         ),
         Node(
             package='rviz2',
             executable='rviz2',
             output='screen'
-        )
+        ),
     ])

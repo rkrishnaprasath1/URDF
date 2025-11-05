efe7ef6b879f2923d2fdbfa6dae3a4988ff1b2d2
rkrishnaprasath1 <rkrishnaprasath1@gmail.com>
2025-11-05 12:04:19 +0530

    Update display.launch.py

--- launch/display.launch.py
+++ launch/display.launch.py
@@ -1,40 +1,17 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
-import os
-
-from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    package_name = 'simple_car_description'
-    urdf_file_name = 'simple_car.urdf'
-
-    urdf_path = os.path.join(
-        get_package_share_directory(package_name),
-        'urdf',
-        urdf_file_name
-    )
-
     return LaunchDescription([
-        Node(
-            package='joint_state_publisher_gui',
-            executable='joint_state_publisher_gui',
-            name='joint_state_publisher_gui',
-            output='screen'
-        ),
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
-            name='robot_state_publisher',
-            output='screen',
-            parameters=[{'robot_description': open(urdf_path).read()}]
+            arguments=['/home/krish/ros2_ws/src/urdf_demo/urdf/car_demo.urdf'],
+            output='screen'
         ),
         Node(
             package='rviz2',
             executable='rviz2',
-            name='rviz2',
             output='screen'
         )
     ])
-
